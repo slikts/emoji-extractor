6afd2327568286720935915a6d0790cd8995f699
slikts <dabas@untu.ms>
2018-09-16 22:34:34 +0300

    usage

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /* eslint-disable no-console, no-void */
 
 const emojiRegex = require(`emoji-regex`)()
@@ -8,10 +9,16 @@ const path = require(`path`)
 
 const [, , codeSource, svgTarget, svgSource] = process.argv
 
+if (!codeSource || !svgTarget || !svgSource) {
+  console.error(`usage: emoji-extrator <code source> <svg target> <svg source>`)
+  process.exit()
+}
+
 const codePoint = symbol => symbol.codePointAt(0).toString(16)
 
 const main = async () => {
   const [sources, svgs] = await Promise.all([glob(codeSource), glob(svgSource)])
+  console.log({ codeSource, svgSource, svgTarget })
   console.log({ sources: sources.length, svgs: svgs.length })
   const emojis = new Set()
   void (await Promise.all(sources.map(name => promises.readFile(name)))).forEach(
